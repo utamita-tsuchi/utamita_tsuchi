835d4a8dfd38ad962c2f3bda9b3be4751c2b4550
hide <hide.twitter.utaite@gmail.com>
2023-10-12 08:14:15 +0900

    修正3

--- tweet.py
+++ tweet.py
@@ -2,13 +2,12 @@
 
 import os
 from datetime import datetime
+import tweepy
 
-import twitter
 
+# Twitterオブジェクトの生成
+auth = tweepy.OAuthHandler(consumer_key=os.environ["API_KEY"], consumer_secret=os.environ["API_SECRET"])
+auth.set_access_token(access_token_key=os.environ["ACCESS_TOKEN"], access_token_secret=os.environ["ACCESS_SECRET"])
+api = tweepy.API(auth)
 
-api = twitter.Api(consumer_key=os.environ["API_KEY"],
-                  consumer_secret=os.environ["API_SECRET"],
-                  access_token_key=os.environ["ACCESS_TOKEN"],
-                  access_token_secret=os.environ["ACCESS_SECRET"]
-                  )
-api.PostUpdate("system time is %s" % datetime.now())
+api.update_status("system time is %s" % datetime.now())
