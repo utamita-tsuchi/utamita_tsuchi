dc116b0441cbe3e2331a774fa90dbbc5fb8683b2
hide <hide.twitter.utaite@gmail.com>
2023-10-12 02:12:52 +0900

    修正2

--- tweet.py
+++ tweet.py
@@ -8,7 +8,7 @@ import twitter
 
 api = twitter.Api(consumer_key=os.environ["API_KEY"],
                   consumer_secret=os.environ["API_SECRET"],
-                  access_token_key=os.environ["ACCESS_TOKEN_KEY"],
-                  access_token_secret=os.environ["ACCESS_TOKEN_SECRET"]
+                  access_token_key=os.environ["ACCESS_TOKEN"],
+                  access_token_secret=os.environ["ACCESS_SECRET"]
                   )
 api.PostUpdate("system time is %s" % datetime.now())
